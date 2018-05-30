0c68a9833d9d646460eb62cffce33c362e5f701a
Hana Lee <leehana@stanford.edu>
2018-05-29 17:20:55 -0700

    updated reg

--- regularized_model.py
+++ regularized_model.py
@@ -5,11 +5,17 @@ import config
 
 model = torch.nn.Sequential(
 			torch.nn.Conv2d(3, 16, 5, padding=2),
+			torch.nn.ReLU(),
+			torch.nn.LayerNorm([16, 32, 32]),
+			torch.nn.Conv2d(16, 32, 5, padding=2),
 			torch.nn.MaxPool2d(2),
 			torch.nn.ReLU(),
-			torch.nn.LayerNorm([16, 16, 16]),
+			torch.nn.LayerNorm([32, 16, 16]),
 			Flatten(),
-			torch.nn.Linear(16*16*16, 100),
+			torch.nn.Linear(32*16*16, 400),
 			torch.nn.ReLU(),
 			torch.nn.Dropout(config.keep_prob),
-			torch.nn.Linear(100, 120))
+			torch.nn.Linear(400, 200),
+			torch.nn.ReLU(),
+			torch.nn.Dropout(config.keep_prob),
+			torch.nn.Linear(200, 120))
